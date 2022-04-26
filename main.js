8c503d510e4b4d97c4cab7843ce6e010da0ef19a
VictorSellan593 <86492929+VictorSellan593@users.noreply.github.com>
2022-04-26 13:48:19 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const setTheme = theme => document.documentElement.className = theme;
+
+
+$(document).ready(function() {
+  $('a[href^="#"]').click(function() {
+    var destino = $(this.hash); //this.hash lee el atributo href de este
+    $('html, body').animate({ scrollTop: destino.offset().top }, 700); //Llega a su destino con el tiempo deseado
+    return false;
+  });
+});
